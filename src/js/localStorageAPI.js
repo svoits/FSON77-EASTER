9211a70cb3c80daf844bf08468cd64b5604603a4
valeri143 <valeria23200523251605@gmail.com>
2023-04-16 16:05:10 +0300

    the end from valeria

--- src/js/localStorageAPI.js
+++ src/js/localStorageAPI.js
@@ -1,9 +1,12 @@
 const STORAGE_KEY = 'note';
 
 function addNote(note) {
-  const notes = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+  const notes = getNotes();
   notes.push(note);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
 }
 
-export default { addNote };
+function getNotes(){
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+}
+export default { addNote, getNotes };
